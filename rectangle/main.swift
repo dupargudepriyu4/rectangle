54d3560315c3649fed310d0ea17e7e626c58d745
Student-001 <student-001@felix-itss-macbook-pro-3.local>
2018-08-03 16:02:26 +0530

    Initial Commit

---
+++ rectangle/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  rectangle
+//
+//  Created by Student-001 on 03/08/18.
+//  Copyright © 2018 minal. All rights reserved.
+
+
+
+import Foundation
+
+print("Hello, World!")
+func add()
+{
+    let a=10,b=20
+    let c=a+b
+    print("addition of \(a) and \(b)is \(c)")
+    
+}
+add()
---
+++ rectangle/product.swift
@@ -0,0 +1,13 @@
+//
+//  product.swift
+//  rectangle
+//
+//  Created by Student-001 on 03/08/18.
+//  Copyright © 2018 minal. All rights reserved.
+//
+
+import Cocoa
+
+class product: NSObject {
+
+}
